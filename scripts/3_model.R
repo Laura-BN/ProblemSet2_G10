668f716ff5c88e8c1238dfc5fa3e5d205fff21b3
Maria Camila Ortiz <mc.ortiza1@uniandes.edu.co>
2025-03-30 23:31:13 -0500

    Primera submission

--- scripts/3_model.R
+++ scripts/3_model.R
@@ -3,76 +3,73 @@
 train <- readRDS(file.path(stores_path, "upsampled_train_data.rds"))
 test  <- readRDS(file.path(stores_path, "test_data.rds"))
 
-summary(train[, c("hacinamiento", "Clase", "jefe_mujer", "jefe_pension", 
-                  "jefe_edad", "jefe_nivel_educ", "N_ocupados", "N_mayor_dependiente")])
-
-summary(test)
-
-# Seleccionar solo las variables deseadas más la variable dependiente
-columnas <- c("hacinamiento", "Clase", "jefe_mujer", "jefe_pension", 
-              "jefe_edad", "jefe_edad2", "jefe_nivel_educ", "N_ocupados",
+# Variables explicativas
+columnas_train <- c("hacinamiento", "Clase", "jefe_mujer", "prop_ocu_pet",
+              "jefe_edad", "jefe_edad2", "jefe_nivel_educ",
               "N_mayor_dependiente", "Pobre")
 
-# Filtrar el dataset
-train_subset <- train[, columnas]
+columnas_test <- c("id", "hacinamiento", "Clase", "jefe_mujer", "prop_ocu_pet", 
+              "jefe_edad", "jefe_edad2", "jefe_nivel_educ", 
+              "N_mayor_dependiente")
 
-# Definir función de F1
-f1_function <- function(data, lev = NULL, model = NULL) {
-   precision <- posPredValue(data$pred, data$obs, positive = lev[1])
-                recall  <- sensitivity(data$pred, data$obs, positive = lev[1])
-                f1_val <- (2 * precision * recall) / (precision + recall)
-                c(F1 = f1_val)
- }
+# Filtrar el dataset
+train_subset <- train[, columnas_train]
+test_subset <- test[, columnas_test]
+
+#Normalizar variables numericas
+train_subset <- train_subset %>%
+  dplyr::mutate_if(is.numeric, scale)
+
+test_subset <- test_subset %>%
+  dplyr::mutate_if(is.numeric, scale)
+
+#Definir función de evaluación personalizada 
+multiStats <- function(...) c(twoClassSummary(...), defaultSummary(...), prSummary(...))
  
-# Configurar `trainControl` para maximizar F1
+#Hiperparámetros usando cross validación 
  ctrl <- trainControl(
    method = "cv",  
    number = 5,  
-   summaryFunction = f1_function, 
+   summaryFunction = multiStats, 
    classProbs = T,  
    verbose = F,  
    savePredictions = T  
  )
  
- # 5 Definición de la grilla de hiperparámetros 
+#Definición de la grilla de hiperparámetros 
  lambda <- 10^seq(2, -6, length = 100)  
- alpha <- seq(0, 1, by = 0.1) 
+ alpha <- seq(0, 1, by = 0.2) 
  grid <- expand.grid("alpha" = alpha, "lambda" = lambda) 
- 
-# 6. Entrenar el modelo usando `glmnet`
-set.seed(1234)
-model1 <- train(
-   Pobre ~ .,  
+
+#Entrenar el modelo usando `glmnet`
+ set.seed(1234)
+ model1 <- train(Pobre~.,
    data = train_subset,  
    method = "glmnet",
    family = "binomial",
    trControl = ctrl,  
-   preProcess = c("center", "scale"),
-   metric = "F1",  
+   metric = "F",  
    tuneGrid = grid
  )
-model1
-
- #7. Calcular predicciones para test
- predictSample <- test %>% 
-   mutate(pobre_pred = predict(model1, newdata = test, type = "raw")) %>% 
-   select(id, pobre_pred)
+ model1
+ 
+#Calcular las predicciones para la base de datos test
+ predictSample <- test_subset %>% 
+   mutate(pobre_pred = predict(model1, newdata = test_subset, type = "raw")) %>% 
+   select(id, pobre_pred) # alpha= 0.2 and lambda = 0.0191791.
  
  predictSample <- predictSample %>% 
-   mutate(pobre=ifelse(pobre_pred=="Yes",1,0)) %>% 
+   mutate(pobre=ifelse(pobre_pred=="Si",1,0)) %>% 
    select(id,pobre)
  
- # Replace '.' with '_' in the numeric values converted to strings
- lambda_str <- gsub(
-   "\.", "_", 
-   as.character(round(model1$bestTune$lambda, 4)))
- alpha_str <- gsub("\.", "_", as.character(model1$bestTune$alpha))
+ table(predictSample$pobre) #31194
  
- name<- paste0(
-   "EN_lambda_", lambda_str,
-   "_alpha_" , alpha_str, 
-   ".csv") 
+ #save
+ lambda_str <- gsub("[.]", "_", as.character(round(model1$bestTune$lambda, 4)))
+ alpha_str <- gsub("[.]", "_", as.character(model1$bestTune$alpha))
  
- write.csv(predictSample,name, row.names = FALSE)
+ name <- paste0("EN_lambda_", lambda_str, "_alpha_", alpha_str, ".csv") 
+ 
+ write.csv(predictSample, file.path(stores_path, name), row.names = FALSE)
 
  
