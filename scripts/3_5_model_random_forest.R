bc99add7ce9d639d51350b45c7cafb48139576e0
CamiloRamirezAs <ca.ramirez10@uniandes.edu.co>
2025-04-03 23:06:35 -0500

    Random forest

--- scripts/3_5_model_random_forest.R
+++ scripts/3_5_model_random_forest.R
@@ -70,6 +70,8 @@ rpart.plot::prp(
             box.palette = "-RdYlGn"  # Paleta de colores para las hojas
             )
 
+# 
+
 # 5. PODAR EL ARBOL USANDO MEJOR CP --------------------------------------------
 
 # Ver tabla de costos y mejor cp
@@ -145,122 +147,6 @@ rpart.plot::prp(
   
   
   
-  #######################################################
-  
-  predictSample <- test_subset %>% 
-    mutate(pobre_pred = predict(model2, newdata = test_subset, type = "raw")) %>% 
-    select(id, pobre_pred) # alpha= 0.6 and lambda = 0.01097
-  
-  predictSample <- predictSample %>% 
-    mutate(pobre=ifelse(pobre_pred=="Si",1,0)) %>% 
-    select(id,pobre)
-  
-  table(predictSample$pobre) #24404 
-  
-  
-  
-  #######################################################
-  
-# Calcular el AUC sobre los datos de prueba
-  
-  pobre <- ifelse(test$Pobre=="Si", 1, 0) #Volder default en test  numérico
-  
-  pred_prob <- predict(cv_tree, newdata = test, type = "prob")   
-
-  aucval_cvtree <- Metrics::auc(actual = pobre, predicted = pred_prob[,1])
-  aucval_cvtree
-  
-  roc_obj <- roc(pobre, pred_prob[,1])
-  auc(roc_obj)
-  
-  colnames(pred_prob)  # Verifica los nombres de las columnas
-  
-  summary(pred_prob[,2])  # Revisa el rango de valores de las predicciones
-  hist(pred_prob[,2])  # Verifica si todas las probabilidades son altas
-  
-  
-  
-  aucval_cvtree <- Metrics::auc(actual = pobre, predicted = pred_prob[,2])
-  print(aucval_cvtree)
-  
-  
-  length(pobre)  
-  length(pred_prob[,2])
-  
-  table(test$Pobre)  # Verifica cuántos "Si" y "No" hay en test
-  class(pobre)  # Debe ser "numeric"
-  sum(is.na(pobre))  # Si hay valores NA, eso puede causar problemas
-
-  head(pred_prob)  # Muestra las primeras filas
-  dim(pred_prob)  # Verifica las dimensiones
-  sum(is.na(pred_prob))  # Revisa si hay valores NA
-  
-  
-  table(pobre) 
-  
-  summary(pred_prob[,2])  # Muestra mínimo, máximo, media, etc.
-  length(unique(pred_prob[,2]))  # Cantidad de valores únicos
-  
-  
-  summary(pred_prob[,2])  # Revisa el rango de valores de las probabilidades
-  hist(pred_prob[,2])  # Gráfica de las predicciones
-  
-  
-# -----------
-
-# Obtener alfa que minimiza el error
-
-  ## Ver tabla de costos y mejor cp
-  printcp(complex_tree)  
-
-  ## Graficar el error vs cp
-  plotcp(complex_tree)
-
-  ## Elegir el cp Óptimo
-  best_cp <- complex_tree$cptable[which.min(complex_tree$cptable[, "xerror"]), "CP"]
-  best_cp
-
-# Podar el arbol con el mejor cp
-
-# Graficar el arbol con alfa que minimiza el error (podar el arbol)
-
-arbol <- rpart(Pobre ~ jefe_edad + jefe_mujer + jefe_edad2 +
-                        N_personas + hacinamiento + N_ocupados + N_inactivos + N_desocupados +
-                        N_menores + N_mayor_dependiente + max_nivel_educ + Clase, 
-                      data = train,
-                      method = "class",
-                      cp = ,  # complexity parameter, nuestro alpha
-                      minbucket = 15 # Numero minimo de obs en hojas
-              )
-
-
-# Graficar el arbol luego de podarlo
-
-rpart.plot::prp(
-            arbol,      
-            under = TRUE,      # Mostrar la información debajo de cada nodo
-            branch.lty = 2,    # Tipo de línea para las ramas (2 = línea punteada)
-            yesno = 2,         # Mostrar indicadores de "sí"/"no"
-            faclen = 0,        # Longitud de la abreviación para niveles de factores (0 = sin abreviación)
-            varlen = 10,       # Longitud máxima para abreviar los nombres de variables
-            box.palette = "-RdYlGn"  # Paleta de colores para las hojas
-          )
-
-
-# Calcular el AUC del arbol
-
-  ## Volver Pobre en test numerico
-  Pobre <- ifelse(test$Pobre=="Pobre", 1, 0)
-  
-  ## Predecri la probabilidad (en lugar de la clase)
-  pred_prob <- predict(arbol, newdata = test, type = "prob")
-  
-  ## Calcular el AUC
-  aucval_arbol <- Metrics::auc(actual = Pobre, predicted = pred_prob[,2]) 
-
-  aucval_arbol
-
-
 
 
  
