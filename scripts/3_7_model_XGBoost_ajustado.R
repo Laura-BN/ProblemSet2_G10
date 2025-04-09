09d3a297bd9fe9a1bc23548ade8b007931acdb6e
CamiloRamirezAs <ca.ramirez10@uniandes.edu.co>
2025-04-08 22:27:28 -0500

    Update 3_7_model_XGBoost_ajustado.R

--- scripts/3_7_model_XGBoost_ajustado.R
+++ scripts/3_7_model_XGBoost_ajustado.R
@@ -58,33 +58,26 @@ table(validation$Pobre)
 
 # Grilla de hiperparámetros con ajustes adicionales
 grid_xgboost <- expand.grid(
-                nrounds = c(100, 250, 500),              # Número de rondas (iteraciones)
+                nrounds = c(100, 250, 500, 1000),              # Número de rondas (iteraciones)
                 max_depth = c(2, 3, 5),                  # Profundidad máxima del árbol
                 eta = c(0.01, 0.05, 0.1),                 # Tasa de aprendizaje
-                gamma = c(0, 0.1, 0.2),                  # Penalización por complejidad
+                gamma = c(0, 0.1, 0.3),                  # Penalización por complejidad
                 min_child_weight = c(1, 10, 25),          # Peso mínimo de un nodo hijo
                 colsample_bytree = c(0.5, 0.7, 1),       # Fracción de características por árbol
                 subsample = c(0.6, 0.8, 1)               # Fracción de muestras para cada árbol
-              )
+              ) %>% 
+              dplyr::filter(!(nrounds == 1000 & eta == 0.1))  # elimina combinaciones pesadas e innecesarias
 
 grid_xgboost
 
 # Definir el cross validation
 
+# Función resumen personalizada con F1
 fiveStats <- function(data, lev = NULL, model = NULL) {
-  require(caret)
-  require(MLmetrics)
-  
-  # AUC, Sensibilidad, Especificidad
-  twoClass <- twoClassSummary(data, lev = lev, model = model)
-  
-  # F1 Score (asegúrate de tener la clase positiva como "Pobre")
-  f1 <- F1_Score(y_true = data$obs, y_pred = data$pred, positive = "Pobre")
-  
-  # Accuracy y Kappa
-  base <- defaultSummary(data, lev = lev, model = model)
-  
-  c(twoClass, F1 = f1, base)
+  c(
+    F1 = MLmetrics::F1_Score(y_pred = data$pred, y_true = data$obs, positive = "Pobre"),
+    caret::twoClassSummary(data, lev, model)
+  )
 }
 
 
@@ -111,7 +104,7 @@ Xgboost_tree <- train(
                 method = "xgbTree", 
                 trControl = fitControl, 
                 tuneGrid = grid_xgboost, 
-                metric = "ROC", 
+                metric = "F1", 
                 verbosity = 0
               )
 
